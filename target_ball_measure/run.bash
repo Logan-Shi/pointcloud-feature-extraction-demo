fc49506f4196e5c848b10e9e558daafc05f137ac
Logan-Shi <837898231@qq.com>
2021-04-09 00:45:02 +0800

    added bayesOpt

--- target_ball_measure/run.bash
+++ target_ball_measure/run.bash
@@ -3,6 +3,8 @@ a = 0
 until [ ! $a -lt $1 ]
 do
 	a=`expr $a + 1`
-    echo "running model/$a.ply for $2 iterations"
-    ./bin/target_ball_measure model/$a.ply $2
+    # echo "running model/$a.ply for $2 iterations"
+    ./bin/target_ball_measure model/$a.ply 10 0.64 0
+    # ./bin/target_ball_measure model/$a.ply 100 0.001 1
+    # ./bin/target_ball_measure model/$a.ply 1000 0.0001 0
 done
--- target_ball_measure/src/target_ball_measure.cpp
+++ target_ball_measure/src/target_ball_measure.cpp
@@ -14,12 +14,72 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <algorithm>
+#include <boost/math/constants/constants.hpp>
+#include <boost/numeric/ublas/assignment.hpp>
+#include "bayesopt/bayesopt.hpp"
+
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 
 bool next_iteration = false;
 bool save_tf = false;
 ofstream results;
+
+// The point clouds we will be using
+PointCloudT::Ptr cloud_in (new PointCloudT);  // Original point cloud
+pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+int iterations = 1;
+double distanceThreshold = 0.001;
+int need_viewer = 0;
+
+class ExampleOneD: public bayesopt::ContinuousModel
+{
+public:
+  ExampleOneD(bayesopt::Parameters par):
+    ContinuousModel(1,par) {}
+
+  double evaluateSample(const vectord& xin)
+  {
+    if (xin.size() != 1)
+      {
+  std::cout << "WARNING: This only works for 1D inputs." << std::endl
+      << "WARNING: Using only first component." << std::endl;
+      }
+
+    double distanceThreshold = xin(0);
+    // Create the segmentation object
+    pcl::SACSegmentation<PointT> seg;
+    // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_SPHERE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (distanceThreshold);
+    seg.setRadiusLimits(14, 16);
+    seg.setMaxIterations (iterations);
+  
+    seg.setInputCloud (cloud_in);
+    // time.tic ();
+    seg.segment (*inliers, *coefficients);
+    // double percentage = double(inliers->indices.size())/cloud_in->size();
+    // double time_consumption = time.toc();
+    // std::cout << "Applied "<<std::to_string(iterations) << " ransac iterations in " << time_consumption << " ms" << std::endl;
+    double radius = coefficients->values[3];
+    return std::abs(radius-15);
+  };
+
+  bool checkReachability(const vectord &query)
+  {return true;};
+
+  void printOptimal()
+  {
+    std::cout << "Optimal:" << 0.23719 << std::endl;
+  }
+};
 
 void
 print4x4Matrix (const Eigen::Matrix4d & matrix)
@@ -47,11 +107,6 @@ int
 main (int argc,
       char* argv[])
 {
-  // The point clouds we will be using
-  PointCloudT::Ptr cloud_in (new PointCloudT);  // Original point cloud
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-
   // Checking program arguments
   if (argc < 2)
   {
@@ -70,96 +125,128 @@ main (int argc,
   }
   std::cout << "\nLoaded file " << argv[1] << " (" << cloud_in->size () << " points) in " << time.toc () << " ms\n" << std::endl;
 
-  int iterations = 1;
   if (argc > 2)
   {
     iterations = atoi(argv[2]);
+    distanceThreshold = strtod(argv[3],NULL);
+    need_viewer = atoi(argv[4]);
     if (iterations < 1)
     {
       PCL_ERROR ("Number of initial iterations must be >= 1.\n");
       return (-1);
     }
   }
-  
-  // Create the segmentation object
-  pcl::SACSegmentation<PointT> seg;
-  // Optional
-  seg.setOptimizeCoefficients (true);
-  // Mandatory
-  seg.setModelType (pcl::SACMODEL_SPHERE);
-  seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold (0.001);
-  seg.setRadiusLimits(14, 16);
-  seg.setMaxIterations (iterations);
 
-  seg.setInputCloud (cloud_in);
-  time.tic ();
-  seg.segment (*inliers, *coefficients);
-  double pencentage = double(inliers->indices.size())/cloud_in->size();
-  std::cout << "Applied "<<std::to_string(iterations) << " ransac iterations in " << time.toc () << " ms" << std::endl;
-
-  pcl::ModelCoefficients sphere_coeff;
-  sphere_coeff.values.resize(4);
-  sphere_coeff.values[0] = coefficients->values[0]; //x
-  sphere_coeff.values[1] = coefficients->values[1]; //y
-  sphere_coeff.values[2] = coefficients->values[2]; //z
-  sphere_coeff.values[3] = coefficients->values[3]; //radius
-
-  // Visualization
-  pcl::visualization::PCLVisualizer viewer ("RANSAC demo");
-
-  // The color we will be using
-  float bckgr_gray_level = 0.0;  // Black
-  float txt_gray_lvl = 1.0 - bckgr_gray_level;
-
-  // Original point cloud is white
-  pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_in_color_h (cloud_in, 180,20,20);
-  viewer.addPointCloud (cloud_in, cloud_in_color_h, "cloud_in");
-  viewer.addSphere(sphere_coeff);
-
-  // Adding text descriptions in each viewport
-  viewer.addText ("Red: Original point cloud\nWhite: Ransac result", 10, 10, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "ransac_info");
-
-  std::stringstream ss;
-  ss << iterations;
-  std::string iterations_cnt = "RANSAC iterations = " + ss.str ();
-  viewer.addText (iterations_cnt, 10, 50, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "iterations_cnt");
-  viewer.addText ("target ball radius: " + std::to_string(coefficients->values[3])+"\n", 10, 70, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "radius");
-  viewer.addText ("Pencentage of inliers: " + std::to_string(pencentage) + "\n", 10, 90, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "inliers pencentage");
-
-  // Set background color
-  viewer.setBackgroundColor (bckgr_gray_level, bckgr_gray_level, bckgr_gray_level);
-
-  // Set camera position and orientation
-  viewer.setCameraPosition (0, 0, 200, 0, 0, 0, 0);
-  viewer.addCoordinateSystem(10);
-  viewer.setSize (1280, 1024);  // Visualiser window size
-
-  // Register keyboard callback :
-  viewer.registerKeyboardCallback (&keyboardEventOccurred, (void*) NULL);
-
-  // Display the visualiser
-  while (!viewer.wasStopped ())
+  if (!need_viewer)
   {
-    viewer.spinOnce ();
+    bayesopt::Parameters parameters;
+    // if(nargs > 1){
+    //   if(!bayesopt::utils::ParamLoader::load(args[1], parameters)){
+    //       std::cout << "ERROR: provided file \"" << args[1] << "\" does not exist" << std::endl;
+    //       return -1;
+    //   }
+    // }
+    // else{
+    parameters.n_init_samples = 10;
+    parameters.n_iterations = 300;
+  
+    parameters.crit_name = "cHedge(cEI,cLCB,cExpReturn,cOptimisticSampling)";
+      //bayesopt::utils::ParamLoader::save("bo_oned.txt", parameters);
+    // }
+    
+    ExampleOneD opt(parameters);
+    vectord result(1);
+    opt.optimize(result);
+    
+    std::cout << "Result:" << result << std::endl;
+  
+  }
+  else
+  {
+    // Create the segmentation object
+    pcl::SACSegmentation<PointT> seg;
+    // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_SPHERE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (distanceThreshold);
+    seg.setRadiusLimits(14, 16);
+    seg.setMaxIterations (iterations);
+  
+    seg.setInputCloud (cloud_in);
+    // time.tic ();
+    seg.segment (*inliers, *coefficients);
+    double percentage = double(inliers->indices.size())/cloud_in->size();
+    double time_consumption = time.toc();
+    std::cout << "Applied "<<std::to_string(iterations) << " ransac iterations in " << time_consumption << " ms" << std::endl;
 
-    // The user pressed "space" :
-    if (next_iteration)
+    results.open("result/test.txt", std::ios_base::app);
+    results << time_consumption << ", "<< percentage << ", " << iterations << ", " << distanceThreshold << ", "<< coefficients->values[3]<<";\n";
+    results.close();
+    pcl::ModelCoefficients sphere_coeff;
+    sphere_coeff.values.resize(4);
+    sphere_coeff.values[0] = coefficients->values[0]; //x
+    sphere_coeff.values[1] = coefficients->values[1]; //y
+    sphere_coeff.values[2] = coefficients->values[2]; //z
+    sphere_coeff.values[3] = coefficients->values[3]; //radius
+  
+    // Visualization
+    pcl::visualization::PCLVisualizer viewer ("target ball demo");
+  
+    // The color we will be using
+    float bckgr_gray_level = 0.0;  // Black
+    float txt_gray_lvl = 1.0 - bckgr_gray_level;
+  
+    // Original point cloud is white
+    pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_in_color_h (cloud_in, 180,20,20);
+    viewer.addPointCloud (cloud_in, cloud_in_color_h, "cloud_in");
+    viewer.addSphere(sphere_coeff);
+  
+    // Adding text descriptions in each viewport
+    viewer.addText ("Red: Original point cloud\nWhite: Ransac result", 10, 10, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "ransac_info");
+  
+    std::stringstream ss;
+    ss << iterations;
+    std::string iterations_cnt = "RANSAC iterations = " + ss.str ();
+    viewer.addText (iterations_cnt, 10, 50, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "iterations_cnt");
+    viewer.addText ("target ball radius: " + std::to_string(coefficients->values[3])+"\n", 10, 70, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "radius");
+    // viewer.addText ("Percentage of inliers: " + std::to_string(percentage) + "\n", 10, 90, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "inliers percentage");
+  
+    // Set background color
+    viewer.setBackgroundColor (bckgr_gray_level, bckgr_gray_level, bckgr_gray_level);
+  
+    // Set camera position and orientation
+    viewer.setCameraPosition (0, 0, 200, 0, 0, 0, 0);
+    viewer.addCoordinateSystem(10);
+    viewer.setSize (1280, 1024);  // Visualiser window size
+  
+    // Register keyboard callback :
+    viewer.registerKeyboardCallback (&keyboardEventOccurred, (void*) NULL);
+    
+    // Display the visualiser
+    while (!viewer.wasStopped ())
     {
-      PCL_ERROR ("\nBad result, next sample.\n");
-      return (-1);
-    }
-    next_iteration = false;
-
-    if (save_tf)
-    {
-      results.open("result/test.txt", std::ios_base::app);
-      // results << "sphere is positioned at: (in " << argv[1]<<" frame)\n";
-      // results << "  Translation vector :\n";
-      results << sphere_coeff.values[0]<< ", " << sphere_coeff.values[1] << ", " << sphere_coeff.values[2]<<";\n";
-      results.close();
-      // std::cout<<"matching finished,exiting...\n";
-      return(0);
+      viewer.spinOnce ();
+  
+      // The user pressed "space" :
+      if (next_iteration)
+      {
+        PCL_ERROR ("\nBad result, next sample.\n");
+        return (-1);
+      }
+      next_iteration = false;
+  
+      if (save_tf)
+      {
+        results.open("result/test.txt", std::ios_base::app);
+        // results << "sphere is positioned at: (in " << argv[1]<<" frame)\n";
+        // results << "  Translation vector :\n";
+        results << sphere_coeff.values[0]<< ", " << sphere_coeff.values[1] << ", " << sphere_coeff.values[2]<<";\n";
+        results.close();
+        // std::cout<<"matching finished,exiting...\n";
+        return(0);
+      }
     }
   }
   return (0);
